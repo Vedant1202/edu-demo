c63826396098bde0ec6c1c18a58a9e972de2f68b
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-05-19 01:00:57 +0530

    Updates

---
+++ precache-manifest.7bc20ccffdc53978b12e2ca8f539fe80.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0f1c847be2c168a0bff56bbe84605c",
+    "url": "/edu-demo/index.html"
+  },
+  {
+    "revision": "7a876c88fbd299ea34f4",
+    "url": "/edu-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6ee84d55667a7a425a7b",
+    "url": "/edu-demo/static/js/2.8a531514.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/edu-demo/static/js/2.8a531514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a876c88fbd299ea34f4",
+    "url": "/edu-demo/static/js/main.afec303c.chunk.js"
+  },
+  {
+    "revision": "042fe986236feea781b5",
+    "url": "/edu-demo/static/js/runtime-main.59d9c139.js"
+  },
+  {
+    "revision": "74fbf6ed307db0ea69086443d4cfc7e7",
+    "url": "/edu-demo/static/media/email.74fbf6ed.png"
+  },
+  {
+    "revision": "7f04139555b7e1aa6a8d71936994d495",
+    "url": "/edu-demo/static/media/intro-illustration_qneuer.7f041395.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/edu-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb1798c6aed1433042db4cfe1678ee87",
+    "url": "/edu-demo/static/media/logo_i5t9wh.fb1798c6.svg"
+  },
+  {
+    "revision": "db6d3e21229e5b322d82b4e8f3ceba6f",
+    "url": "/edu-demo/static/media/stack.db6d3e21.png"
+  }
+]);
